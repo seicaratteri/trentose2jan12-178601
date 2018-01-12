8e3ff2791845d7a332fa043700a969cb0c02de43
seicaratteri <pietro.bolcato@studenti.unitn.it>
2018-01-12 10:47:51 +0100

    getArea improvement

--- area.js
+++ area.js
@@ -6,17 +6,20 @@ const area = express.Router()
 function getArea(arr)
 {
     let ret = -1;
-    if (arr.length == 2)
+    if (typeof arr != 'undefined')
     {
-        let x = arr[0];
-        let y = arr[1];
-
-        if (!isNaN(x) && !isNaN(y))
+        if (arr.length == 2)
         {
-            if (x >= 0 && y >= 0)
-                ret = x*y;
+            let x = arr[0];
+            let y = arr[1];
+
+            if (Number.isInteger(x) && Number.isInteger(y))
+            {
+                if (x >= 0 && y >= 0)
+                    ret = x*y;
+            }
         }
-    }
+    }   
 
     return ret;
 }
