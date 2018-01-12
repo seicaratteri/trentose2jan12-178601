398a3aeda127c89ec9b2d7db651f027263ddf954
seicaratteri <pietro.bolcato@studenti.unitn.it>
2018-01-12 10:45:41 +0100

    getArea improvement
    Controlla se si passa una varabile o meno

--- area.js
+++ area.js
@@ -1,19 +1,22 @@
 function getArea(arr)
 {
 	let ret = -1;
-	if (arr.length == 2)
-	{
-		let x = arr[0];
-		let y = arr[1];
+    if (typeof arr != 'undefined')
+    {
+        if (arr.length == 2)
+        {
+            let x = arr[0];
+            let y = arr[1];
 
-		if (!isNaN(x) && !isNaN(y))
-		{
-			if (x >= 0 && y >= 0)
-				ret = x*y;
-		}
-	}
+            if (Number.isInteger(x) && Number.isInteger(y))
+            {
+                if (x >= 0 && y >= 0)
+                    ret = x*y;
+            }
+        }
+    }	
 
 	return ret;
 }
 
-console.log(getArea([2]));
+module.exports = getArea;
